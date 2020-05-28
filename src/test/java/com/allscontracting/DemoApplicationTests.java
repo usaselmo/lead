dc8c9cb9d9d2bb64633374b2626a25946e322a2b
Anselmo Ribeiro <6975.usa@gmail.com>
2020-05-27 23:42:55 -0400

    create proposal report

--- src/test/java/com/allscontracting/DemoApplicationTests.java
+++ src/test/java/com/allscontracting/DemoApplicationTests.java
@@ -1,9 +1,14 @@
 package com.allscontracting;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.time.LocalDateTime;
 import java.util.Date;
+import java.util.HashMap;
 
 import org.junit.Ignore;
 import org.junit.Test;
@@ -17,7 +22,11 @@ import com.allscontracting.event.EventManager;
 import com.allscontracting.event.VisitScheduledEvent;
 import com.allscontracting.model.Client;
 import com.allscontracting.model.Lead;
+import com.allscontracting.model.Proposal;
+import com.allscontracting.repo.ProposalRepository;
 import com.allscontracting.service.LeadService;
+
+import net.sf.jasperreports.engine.JasperRunManager;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -27,15 +36,18 @@ public class DemoApplicationTests {
 	@Autowired EventManager eventManager;
 
 	@Test
+	@Ignore
 	public void contextLoads() {
 	}
 	
 	@Test
+	@Ignore
 	public void init() throws Exception {
 		assertNotNull(this.leadService);
 	}
 	
 	@Test
+	@Ignore
 	public void changeLeadEvent() throws Exception {
 		LocalDateTime dateTime;
 		Lead lead;
@@ -48,6 +60,28 @@ public class DemoApplicationTests {
 	@Ignore
 	public void testEvent() throws Exception {
 		this.eventManager.notifyAllListeners(new VisitScheduledEvent(Lead.builder().id("sdfsd").build(), Client.builder().id(125L).build(), new Date()));
+	}
+
+	private static final String JASPER_FOLDER = "jasper/";
+	private static final String JASPER_SUFFIX = ".jasper";
+
+	@Autowired ProposalRepository proposalRepo;
+	
+	@Test
+	public void testReport() throws Exception {
+		
+		Proposal proposal = this.proposalRepo.findAll().get(0);
+		Client client = proposal.getLead().getClient();
+
+		String fileName = JASPER_FOLDER + "proposal" + JASPER_SUFFIX;
+		String sourceFile = ReportTest.class.getClassLoader().getResource(fileName).getPath().replaceFirst("/", "");
+		String destFile = "D:/temp/proposal.pdf";
+		
+		HashMap<String, Object> map = new HashMap<>();
+		map.put("CLIENT", client);
+		map.put("PROPOSAL", proposal);
+		
+		JasperRunManager.runReportToPdfFile(sourceFile, destFile, map);
 	}
 	
 }
