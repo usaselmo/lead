0ef84039c483580e7a987ed44ecf032d2a97d41d
Anselmo Ribeiro <6975.usa@gmail.com>
2020-06-11 16:33:50 -0400

    report working

--- src/main/java/com/allscontracting/service/ReportService.java
+++ src/main/java/com/allscontracting/service/ReportService.java
@@ -3,17 +3,17 @@ package com.allscontracting.service;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.sql.SQLException;
 import java.util.HashMap;
 
-import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ResourceUtils;
 
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperFillManager;
@@ -32,56 +32,41 @@ public class ReportService {
 	private static final String JASPER_SUFFIX = ".jasper";
 
 	public void getReportAsRtfStream(HttpServletResponse response, HashMap<String, Object> map, String streamFileName,
-		String jasperReportFileName) throws IOException, JRException, SQLException {
-
-    final ByteArrayOutputStream baos = new ByteArrayOutputStream();
-    final JasperPrint jasperPrint = JasperFillManager.fillReport(getSourceFileName(jasperReportFileName), map, dataSource.getConnection());
-    final JRRtfExporter saida = new JRRtfExporter();
-    saida.setExporterInput(new SimpleExporterInput(jasperPrint));
-    saida.setExporterOutput(new SimpleWriterExporterOutput(baos,"UTF-8"));
-    saida.exportReport();
-    byte[] bytes = baos.toByteArray();
-
-    response.setContentType("application/rtf");
-    response.setHeader("content-disposition", "attachment; filename=\"" + streamFileName + "\"");
-    response.setContentLength(bytes.length);
-    response.getOutputStream().write(bytes, 0, bytes.length);
-    response.getOutputStream().flush();
-    response.getOutputStream().close();
+			String jasperReportFileName) throws IOException, JRException, SQLException {
+		final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		final JasperPrint jasperPrint = JasperFillManager.fillReport(getSourceFileName(jasperReportFileName), map,
+				dataSource.getConnection());
+		final JRRtfExporter saida = new JRRtfExporter();
+		saida.setExporterInput(new SimpleExporterInput(jasperPrint));
+		saida.setExporterOutput(new SimpleWriterExporterOutput(baos, "UTF-8"));
+		saida.exportReport();
+		byte[] bytes = baos.toByteArray();
+		response.setContentType("application/rtf");
+		response.setHeader("content-disposition", "attachment; filename=\"" + streamFileName + "\"");
+		response.setContentLength(bytes.length);
+		response.getOutputStream().write(bytes, 0, bytes.length);
+		response.getOutputStream().flush();
+		response.getOutputStream().close();
 
 	}
 
 	public void getReportAsPdfStream(HttpServletResponse response, HashMap<String, Object> map, String streamFileName,
 		String jasperReportFileName) throws JRException, SQLException, IOException, Exception {
-		
-		String sourceFileName = getSourceFileName(jasperReportFileName);
-		String tempFileName = Files.createTempFile("", "").toFile().getAbsolutePath();
-
-		JasperRunManager.runReportToPdfFile(sourceFileName, tempFileName, map, dataSource.getConnection());
-		byte[] byteArray = Files.readAllBytes(Paths.get(tempFileName));
-		
+		InputStream is = this.getClass().getClassLoader().getResourceAsStream(JASPER_FOLDER+jasperReportFileName+ JASPER_SUFFIX);
 		response.setContentType("application/pdf");
 		response.setHeader("content-disposition", "attachment; filename=\"" + streamFileName + "\"");
-		response.setContentLength(byteArray.length);
-		ServletOutputStream os = response.getOutputStream();
-		try {
-			os.write(byteArray, 0, byteArray.length);
-		} catch (Exception excp) {
-			throw excp;
-		} finally {
-			os.close();
-		}
+		JasperRunManager.runReportToPdfStream(is, response.getOutputStream(), map, dataSource.getConnection());
 	}
 
-	private String getSourceFileName(String jasperReportFileName) {
+	private String getSourceFileName(String jasperReportFileName) throws IOException {
 		String fileName = JASPER_FOLDER + jasperReportFileName + JASPER_SUFFIX;
-		String sourceFile = ProposalService.class.getClassLoader().getResource(fileName).getPath().replaceFirst("/", "");
-		return sourceFile;
+		String sourceFile = this.getClass().getClassLoader().getResource(fileName).getFile(); 
+		return sourceFile;  
 	}
 
 	public File getReportAsPdfFile(String fileName, HashMap<String, Object> map,	String jasperReportFileName) throws JRException, SQLException, IOException {
 		String jasperfileName = JASPER_FOLDER + jasperReportFileName + JASPER_SUFFIX;
-		String sourceFile = ProposalService.class.getClassLoader().getResource(jasperfileName).getPath().replaceFirst("/", "");
+		String sourceFile = ResourceUtils.getFile(jasperfileName).getAbsolutePath(); //new ClassPathResource(jasperfileName).getPath();
 		File tmpFile = Files.createTempFile("", fileName).toFile();
 		JasperRunManager.runReportToPdfFile(sourceFile, tmpFile.getAbsolutePath(), map, dataSource.getConnection());
 		return tmpFile;
