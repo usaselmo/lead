314d143a548fd5d40fc2810e6bb502af52c5d66a
Anselmo Ribeiro <6975.usa@gmail.com>
2020-06-13 17:38:40 -0400

    dto

--- src/main/java/com/allscontracting/dto/ResponseEntity.java
+++ src/main/java/com/allscontracting/dto/ResponseEntity.java
@@ -2,25 +2,44 @@ package com.allscontracting.dto;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import com.allscontracting.model.Lead;
+
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ResponseEntity {
 
 	private final List<String> successMessages = new ArrayList<>();
 	private final List<String> errorMessages = new ArrayList<>();
 	private LeadDTO lead;
+	private final List<LeadDTO> leads = new ArrayList<>();
 	
-	
-	public void addSuccessMessage(String successMessage) {
+	public ResponseEntity addSuccessMessage(String successMessage) {
 		this.successMessages.add(successMessage);
+		return this;
 	}
 	
-	public void addErrorMessage(String errorMessage) {
+	public ResponseEntity addErrorMessage(String errorMessage) {
 		this.errorMessages.add(errorMessage);
+		return this;
+	}
+	
+	public ResponseEntity addLead(Lead lead) {
+		this.leads.add(LeadDTO.leadToDTO(lead));
+		return this;
+	}
+	
+	public ResponseEntity addAllLeads(List<Lead> leads) {
+		this.leads.addAll(leads.stream().map(l->LeadDTO.leadToDTO(l)).collect(Collectors.toList()));
+		return this;
 	}
 	
 }
