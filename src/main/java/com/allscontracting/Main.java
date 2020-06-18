82265c48ac438dfbff580549ff0cb371f509cdf5
Anselmo Ribeiro <6975.usa@gmail.com>
2020-06-18 18:23:50 -0400

    refactor

--- src/main/java/com/allscontracting/Main.java
+++ src/main/java/com/allscontracting/Main.java
@@ -3,9 +3,6 @@ package com.allscontracting;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class Main implements CommandLineRunner {
@@ -16,15 +13,9 @@ public class Main implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println(this.passwordEncoder().encode("123"));
+		//System.out.println(this.passwordEncoder().encode("123"));
 	}
 
-
-  
-  @Bean
-  public PasswordEncoder passwordEncoder() {
-    return new BCryptPasswordEncoder();
-  }
   
   
 }
