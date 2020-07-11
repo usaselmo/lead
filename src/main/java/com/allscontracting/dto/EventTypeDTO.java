b9e33dfe29e9724677bf30e451189c6ed416addd
Anselmo Ribeiro <6975.usa@gmail.com>
2020-07-11 08:49:07 -0400

    working on event type

--- src/main/java/com/allscontracting/dto/EventTypeDTO.java
+++ src/main/java/com/allscontracting/dto/EventTypeDTO.java
@@ -19,19 +19,21 @@ public class EventTypeDTO {
 
 	private String name;
 	private String status ; 
-	private String action ; 
+	private String action ;
+	private String abbreviation;
 	
 	public static EventTypeDTO of(EventType event) {
 		return EventTypeDTO.builder()
 				.name(event.name())
 				.action(event.getAction())
 				.status(event.getStatus())
+				.abbreviation(event.getAbbreviation())
 				.build();
 	}
 	
 	public static EventType toEventType(EventTypeDTO eventTypeDTO) {
 		if(eventTypeDTO==null)
-			Objects.requireNonNull("Event can not be null");
+			Objects.requireNonNull("Event Type can not be null");
 		if(StringUtils.isNotBlank(eventTypeDTO.getAction()))
 			return EventType.valueOf(eventTypeDTO.getAction());
 		else
--- src/main/java/com/allscontracting/event/EventType.java
+++ src/main/java/com/allscontracting/event/EventType.java
@@ -4,26 +4,34 @@ import java.util.stream.Stream;
 
 public enum EventType {
 	
-	BEGIN("Started", "Begin"),
-	CONTACT_QUALIFY("Contact and Qualify", "Contact Client"),
-	ASSIGN_TO_ESTIMATOR("Assign to Estimator", "Assign to Estimator"),
-	SCHEDULE_VISIT("Visit Scheduled", "Schedule a Visit"),
-	MARK_AS_VISITED("Marked as Visited", "Mark as Visited"),
-	CREATE_PROPOSAL("Proposal Created", "Create Proposal"),
-	SEND_PROPOSAL("Proposal Sent", "Send Proposal"),
-	BEGIN_WORK("Work Started", "Begin Work"),
-	FINISH_WORK("Work Finished", "Finish Work"),
-	END_LEAD("Ended", "End Lead"),
-	LOAD_VENDOR_FILE("Vendor File Loaded", "Load Vendor File");
+	BEGIN("Started", "Begin", "BE"),
+	CONTACT_QUALIFY("Contact and Qualify", "Contact Client", "CQ"),
+	ASSIGN_TO_ESTIMATOR("Assign to Estimator", "Assign to Estimator", "AE"),
+	SCHEDULE_VISIT("Visit Scheduled", "Schedule a Visit", "SV"),
+	MARK_AS_VISITED("Marked as Visited", "Mark as Visited", "MV"),
+	CREATE_PROPOSAL("Proposal Created", "Create Proposal", "CP"),
+	SEND_PROPOSAL("Proposal Sent", "Send Proposal", "SP"),
+	BEGIN_WORK("Work Started", "Begin Work", "BW"),
+	FINISH_WORK("Work Finished", "Finish Work", "FW"),
+	END_LEAD("Ended", "End Lead", "EL"),
+	LOAD_VENDOR_FILE("Vendor File Loaded", "Load Vendor File", "LVF");
 	
 	private String status;
 	private String action;
+	private String abbreviation;
 	
-	EventType(String status, String action) {
+	EventType(String status, String action, String abbreviation) {
 		this.status = status;
 		this.action = action;
+		this.abbreviation = abbreviation;
 	}
 	
+	public String getAbbreviation() {
+		return abbreviation;
+	}
+
+
+
 	public String getStatus() {
 		return this.status;
 	}
