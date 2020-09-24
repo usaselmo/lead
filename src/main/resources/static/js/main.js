9ecb6104b8dd4c4a9929e1d3df9e649d240deef0
Anselmo S Ribeiro <anselmo.sr@gmail.com>
2020-09-24 18:09:04 -0400

    refactoring file names

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,17 @@
+/*
+ * ANGULARJS 
+ */
+ var local_server_url = "";
+
+ var app = angular.module('main', [
+ 	'app.module.lead',
+ 	'app.module.header',
+ 	'app.module.menu',
+ 	]);
+
+
+ /* MAIN CONTROLLER */
+app.controller('MainController', function ($scope) {  
+
+} );
+
