7e31d7762819db08a7e1113e3447a37a6f62df22
Anselmo S Ribeiro <anselmo.sr@gmail.com>
2020-09-25 16:35:00 -0400

    lead proposal controller

--- src/main/resources/static/app/module/lead/proposal/lead-proposal-crud.js
+++ src/main/resources/static/app/module/lead/proposal/lead-proposal-crud.js
@@ -14,36 +14,38 @@ proposalCrud.directive('proposalCrud', function() {
 			lead: '=',
 		},
 		templateUrl: '/app/module/lead/proposal/lead-proposal-crud.html' , 
-		controller: function($scope, proposalService, leadService){
-			
-			$scope.proposalCancel = function(){
-		 	//$scope.proposal = null;
-		 }
+		controller: proposalCrudController,
+	}; 
+} );
 
-		 $scope.proposalEncreaseItem = function(proposal){
-		 	$scope.proposal.items.push({price: 0})
-		 }
+var proposalCrudController = function($scope, proposalService, leadService) {
+	
+	$scope.proposalCancel = function(){
+ 	// $scope.proposal = null;
+ }
 
-		 $scope.proposalRemoveItem = function(proposal){
-		 	if($scope.proposal.items.length > 1 ){
-		 		$scope.proposal.items.pop();
-		 	}
-		 }
+ $scope.proposalEncreaseItem = function(proposal){
+ 	$scope.proposal.items.push({price: 0})
+ }
 
-		 $scope.proposalSave = function(lead, proposal){
-		 	if(proposal.id){
-		 		proposalService.update($scope, proposal, lead.id)
-		 		.success( (response) => {
-		 			$scope.lead.proposals = $scope.lead.proposals.filter(p=>p.id != proposal.id)
-		 			$scope.lead.proposals.push(response.proposal)
-		 		} );
-		 	}else{
-		 		proposalService.save($scope, proposal, lead.id)
-		 	}
-		 }
+ $scope.proposalRemoveItem = function(proposal){
+ 	if($scope.proposal.items.length > 1 ){
+ 		$scope.proposal.items.pop();
+ 	}
+ }
 
-		},
-	};
-});
+ $scope.proposalSave = function(lead, proposal){
+ 	if(proposal.id){
+ 		proposalService.update($scope, proposal, lead.id)
+ 		.success( (response) => {
+ 			$scope.lead.proposals = $scope.lead.proposals.filter(p=>p.id != proposal.id)
+ 			$scope.lead.proposals.push(response.proposal)
+ 		} );
+ 	}else{
+ 		proposalService.save($scope, proposal, lead.id)
+ 	}
+ }
+
+};
 
 export default proposalCrud
