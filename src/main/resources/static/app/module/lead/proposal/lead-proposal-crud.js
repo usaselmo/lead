dd1bd0ff7e2ebe11e436fa4bc6531b36b2a403c2
Anselmo S Ribeiro <anselmo.sr@gmail.com>
2020-09-26 15:26:10 -0400

    improve some js features

--- src/main/resources/static/app/module/lead/proposal/lead-proposal-crud.js
+++ src/main/resources/static/app/module/lead/proposal/lead-proposal-crud.js
@@ -1,51 +1,42 @@
 import leadService from '/app/service/lead-service.js'
 import proposalService from '/app/service/proposal-service.js'
-
 var proposalCrud = angular.module('app.module.lead.proposal.crud', []);
-
 proposalCrud.service('proposalService', proposalService);
 proposalCrud.service('leadService', leadService);
-
 proposalCrud.directive('proposalCrud', function() {
-	return {
-		restrict: 'E',
-		scope: {
-			proposal: '=', 
-			lead: '=',
-		},
-		templateUrl: '/app/module/lead/proposal/lead-proposal-crud.html' , 
-		controller: proposalCrudController,
-	}; 
-} );
-
+    return {
+        restrict: 'E',
+        scope: {
+            proposal: '=',
+            lead: '=',
+        },
+        templateUrl: '/app/module/lead/proposal/lead-proposal-crud.html',
+        controller: proposalCrudController,
+    };
+});
 var proposalCrudController = function($scope, proposalService, leadService) {
-	
-	$scope.proposalCancel = function(){
- 	// $scope.proposal = null;
- }
-
- $scope.proposalEncreaseItem = function(proposal){
- 	$scope.proposal.items.push({price: 0})
- }
-
- $scope.proposalRemoveItem = function(proposal){
- 	if($scope.proposal.items.length > 1 ){
- 		$scope.proposal.items.pop();
- 	}
- }
-
- $scope.proposalSave = function(lead, proposal){
- 	if(proposal.id){
- 		proposalService.update($scope, proposal, lead.id)
- 		.success( (response) => {
- 			$scope.lead.proposals = $scope.lead.proposals.filter(p=>p.id != proposal.id)
- 			$scope.lead.proposals.push(response.proposal)
- 		} );
- 	}else{
- 		proposalService.save($scope, proposal, lead.id)
- 	}
- }
-
+    $scope.proposalCancel = function() {
+        // $scope.proposal = null;
+    }
+    $scope.proposalEncreaseItem = function(proposal) {
+        $scope.proposal.items.push({
+            price: 0, title: 'ITEM '+(proposal.items.length+1)+' - '
+        })
+    }
+    $scope.proposalRemoveItem = function(proposal) {
+        if ($scope.proposal.items.length > 1) {
+            $scope.proposal.items.pop();
+        }
+    }
+    $scope.proposalSave = function(lead, proposal) {
+        if (proposal.id) {
+            proposalService.update($scope, proposal, lead.id).success((response) => {
+                $scope.lead.proposals = $scope.lead.proposals.filter(p => p.id != proposal.id)
+                $scope.lead.proposals.push(response.proposal)
+            });
+        } else {
+            proposalService.save($scope, proposal, lead.id)
+        }
+    }
 };
-
 export default proposalCrud
--- src/main/resources/static/app/module/lead/proposal/lead-proposal.js
+++ src/main/resources/static/app/module/lead/proposal/lead-proposal.js
@@ -1,91 +1,75 @@
 import proposalCrud from '/app/module/lead/proposal/lead-proposal-crud.js';
 import proposalService from '/app/service/proposal-service.js'
-
-var copy = function (obj) {
-	return JSON.parse(JSON.stringify(obj))
+var copy = function(obj) {
+    return JSON.parse(JSON.stringify(obj))
 };
-var leadProposal = angular.module('app.module.lead.proposal', [
-	'app.module.lead.proposal.crud',
-	]);
-
+var leadProposal = angular.module('app.module.lead.proposal', ['app.module.lead.proposal.crud', ]);
 leadProposal.service('proposalService', proposalService);
-
 leadProposal.directive('appLeadProposal', function() {
-	var originalLines = [];
-	return {
-		retrict: 'E',
-		templateUrl: '/app/module/lead/proposal/lead-proposal.html',
-		scope: {
-			lead: '=',
-		},
-		controller: ['$scope', 'proposalService', function($scope, proposalService) {
-
-			$scope.originalLines = [];
-			
-			var convertToClientFormat = function(proposal) {
-				var prop = copy(proposal)
-				var items = []
-				prop.items.forEach(item => {
-					item.lines = item.lines.map(line => {
-						$scope.originalLines[item.id + line.description] = line.id;
-						return line.description;
-					}).join("\n")
-					items.push(item)
-				})
-				prop.items = items;
-				return prop;
-			}
-
-			$scope.emailProposal = function(proposal) {
-				if (confirm('Send Proposal #' + proposal.number + ' via E-mail ? ')) {
-					proposalService.sendByEmail($scope, proposal)
-				}
-			}
-
-			$scope.proposalCopy = function(proposal) {
-				var nproposal = convertToClientFormat(proposal);
-				nproposal.total = 0;
-				nproposal.id = null;
-				nproposal.emailed = false;
-				nproposal.number = null;
-				nproposal.items.forEach(item => {
-					item.id = null;
-				})
-				originalLines = [];
-				$scope.proposal = nproposal;
-			}
-
-			$scope.proposalCrud = function(proposal, lead) {
-				if (!proposal.id)
-					proposal = createProposal(lead);
-				else
-					proposal = convertToClientFormat(proposal);
-				$scope.proposal = proposal;
-			}
-
-			var createProposal = function () {
-				var prop = {
-					'items': [{'price':0}],
-					'callMissUtility': true,
-					'scopeOfWork': 'Concrete work as per visit',
-					'paymentSchedule': '1. 50% down payment upon start of the project\n2. 50% final payment upon completion of the project.',
-					'workWarranty': 'All new material and labor are guaranteed for 36 months from completion date. All work to be completed in a neat and workmanlike manner. Warranty applies for concrete cracks that are 3/8" or greater in separation or height difference for flat concrete work. Warranty excludes the following: concrete damage caused by deicers such as salt or any deicer or fertilizer containing ammonium nitrate or ammonium sulfate, concrete spider cracks, hairline cracks and color variance.',
-					'total': 0,	
-				}
-				$scope.currentProposal = prop;
-				return prop;
-			}
-
-			$scope.deleteProposal = function(lead, proposal) {
-				if (confirm(' Are you sure you want to delete? ')) {
-					proposalService.delete($scope, proposal, lead.id)
-				}
-			}
-
-
-
-		}],
-	}
+    var originalLines = [];
+    return {
+        retrict: 'E',
+        templateUrl: '/app/module/lead/proposal/lead-proposal.html',
+        scope: {
+            lead: '=',
+        },
+        controller: ['$scope', 'proposalService', function($scope, proposalService) {
+            $scope.originalLines = [];
+            var convertToClientFormat = function(proposal) {
+                var prop = copy(proposal)
+                var items = []
+                prop.items.forEach(item => {
+                    item.lines = item.lines.map(line => {
+                        $scope.originalLines[item.id + line.description] = line.id;
+                        return line.description;
+                    }).join("\n")
+                    items.push(item)
+                })
+                prop.items = items;
+                return prop;
+            }
+            $scope.emailProposal = function(proposal) {
+                if (confirm('Send Proposal #' + proposal.number + ' via E-mail ? ')) {
+                    proposalService.sendByEmail($scope, proposal)
+                }
+            }
+            $scope.proposalCopy = function(proposal) {
+                var nproposal = convertToClientFormat(proposal);
+                nproposal.total = 0;
+                nproposal.id = null;
+                nproposal.emailed = false;
+                nproposal.number = null;
+                nproposal.items.forEach(item => {
+                    item.id = null;
+                })
+                originalLines = [];
+                $scope.proposal = nproposal;
+            }
+            $scope.proposalCrud = function(proposal, lead) {
+                if (!proposal.id) proposal = createProposal(lead);
+                else proposal = convertToClientFormat(proposal);
+                $scope.proposal = proposal;
+            }
+            var createProposal = function() {
+                var prop = {
+                    'items': [{
+                        'price': 0, title: 'ITEM 1 - '
+                    }],
+                    'callMissUtility': true,
+                    'scopeOfWork': 'Concrete work as per visit',
+                    'paymentSchedule': '1. 50% down payment upon start of the project\n2. 50% final payment upon completion of the project.',
+                    'workWarranty': 'All new material and labor are guaranteed for 36 months from completion date. All work to be completed in a neat and workmanlike manner. Warranty applies for concrete cracks that are 3/8" or greater in separation or height difference for flat concrete work. Warranty excludes the following: concrete damage caused by deicers such as salt or any deicer or fertilizer containing ammonium nitrate or ammonium sulfate, concrete spider cracks, hairline cracks and color variance.',
+                    'total': 0,
+                }
+                $scope.currentProposal = prop;
+                return prop;
+            }
+            $scope.deleteProposal = function(lead, proposal) {
+                if (confirm(' Are you sure you want to delete? ')) {
+                    proposalService.delete($scope, proposal, lead.id)
+                }
+            }
+        }],
+    }
 })
-
 export default proposalCrud
