4025acbe0c59353fabd6b589a4d6b2db67d9b9a1
Anselmo S Ribeiro <anselmo.sr@gmail.com>
2020-09-29 18:11:47 -0400

    create class filter

---
+++ src/main/resources/static/app/model/filter.js
@@ -0,0 +1,10 @@
+export default class Filter {
+
+    constructor(pageRange, lines, searchText, event){
+        this.pageRange = pageRange
+        this.lines = lines
+        this.searchText = searchText
+        this.event = event
+    }
+
+}
