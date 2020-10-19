a429080ce750f3c0a064f8ff55d130a4b43d927e
Anselmo S Ribeiro <anselmo.sr@gmail.com>
2020-10-19 10:23:03 -0400

    create cors filter in Main class

--- src/main/java/com/allscontracting/Main.java
+++ src/main/java/com/allscontracting/Main.java
@@ -2,6 +2,10 @@ package com.allscontracting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class Main {
@@ -9,5 +13,26 @@ public class Main {
 	public static void main(String[] arguments) {
 		SpringApplication.run(Main.class, arguments);
 	}
-
+	
+	@Bean
+	public CorsFilter corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("OPTIONS");
+		config.addAllowedMethod("GET");
+		config.addAllowedMethod("POST");
+		config.addAllowedMethod("PUT");
+		config.addAllowedMethod("DELETE");
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
+	}
+	
+	
+	
+	
+	
+	
 }
