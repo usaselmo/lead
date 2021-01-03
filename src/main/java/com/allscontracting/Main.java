1d09f072eaaa3c698d71b21ee52f40005ada4563
Anselmo S Ribeiro <anselmo.sr@gmail.com>
2021-01-03 16:00:11 -0300

    cors

--- src/main/java/com/allscontracting/Main.java
+++ src/main/java/com/allscontracting/Main.java
@@ -3,9 +3,8 @@ package com.allscontracting;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Main {
@@ -14,25 +13,27 @@ public class Main {
 		SpringApplication.run(Main.class, arguments);
 	}
 	
-	@Bean
-	public CorsFilter corsFilter() {
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowCredentials(true);
-		config.addAllowedOrigin("*");
-		config.addAllowedHeader("*");
-		config.addAllowedMethod("OPTIONS");
-		config.addAllowedMethod("GET");
-		config.addAllowedMethod("POST");
-		config.addAllowedMethod("PUT");
-		config.addAllowedMethod("DELETE");
-		source.registerCorsConfiguration("/**", config);
-		return new CorsFilter(source);
-	}
+	/*
+	 * @Bean public CorsFilter corsFilter() { UrlBasedCorsConfigurationSource source
+	 * = new UrlBasedCorsConfigurationSource(); CorsConfiguration config = new
+	 * CorsConfiguration(); config.setAllowCredentials(true);
+	 * config.addAllowedOrigin("*"); config.addAllowedHeader("*");
+	 * config.addAllowedMethod("OPTIONS"); config.addAllowedMethod("GET");
+	 * config.addAllowedMethod("POST"); config.addAllowedMethod("PUT");
+	 * config.addAllowedMethod("DELETE"); source.registerCorsConfiguration("/**",
+	 * config); return new CorsFilter(source); }
+	 */
 	
 	
-	
-	
-	
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+      return new WebMvcConfigurer() {
+          @Override
+          public void addCorsMappings(CorsRegistry registry) {
+              registry.addMapping("/**");
+          }
+      };
+  }
+		
 	
 }
