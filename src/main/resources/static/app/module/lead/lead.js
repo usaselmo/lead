0f617ba2d28fd4427954f295b405c2a2f9128112
Anselmo S Ribeiro <anselmo.sr@gmail.com>
2021-03-03 12:11:53 -0300

    list lines per page from 100 to 10

--- src/main/resources/static/app/module/lead/lead.js
+++ src/main/resources/static/app/module/lead/lead.js
@@ -48,7 +48,7 @@ export default applead
 
 var leadController = function ($scope, leadService, companyService, personService, userService) {
 
-	$scope.filter = new Filter(0, 100, '', '');
+	$scope.filter = new Filter(0, 10, '', '');
 	$scope.lead = null;
 	$scope.leads = {};
 
